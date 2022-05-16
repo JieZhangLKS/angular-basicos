8be8be54c6ea81d44b77db4f0250003e2a3455b4
JieZhangLKS <jiezhang@lksnext.com>
2022-05-16 15:03:15 +0200

    primer commit

---
+++ src/app/contadores/contador/contador.component.ts
@@ -0,0 +1,24 @@
+import { Component } from "@angular/core";
+@Component({
+    selector: 'app-contador',
+    template:`
+        <h1>{{titulo}}</h1>
+
+        <h3>La base es: <strong> {{base}} </strong> </h3>
+
+        <button (click)="acum(base)"> +{{base}}</button>
+
+        <span>{{numero}}</span>
+
+        <button (click)="acum(-base)"> -{{base}}</button>
+    `
+})
+export class ContadorComponent {
+    public titulo: string = 'Contador App';
+    public numero: number = 0;
+    public base: number = 5;
+  
+    acum( valor: number) {
+      this.numero+=valor;
+    }
+}
